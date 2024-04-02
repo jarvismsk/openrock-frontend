ab7656c111f3801f069e9ed32d6f334b7ecb54bf
I Kha <122979963+jarvismsk@users.noreply.github.com>
2024-04-02 19:31:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,8 @@ const HomePage = () => {
         
         <HeroSection />
         <div className="container mx-auto px-4 md:px-8 py-12 md:py-20">
-          <h2 className="text-xl md:text-3xl lg:text-4xl font-semibold mb-6 md:mb-8 text-left md:text-left md:pl-4 text-black">
-            Realtime Datasets:
-          </h2>
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-            <DatasetsPage />
-          </div>
+   
+    
           <div>
             <ComingList />
             <Analytics />
