e84d49a259b1c9892ef68db6fb8e5614cca4fbe7
I Kha <122979963+jarvismsk@users.noreply.github.com>
2024-04-02 19:30:21 +0530

    Update herosection.js

--- components/herosection.js
+++ components/herosection.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <div className="py-10 md:py-5 px-4 relative overflow-hidden">
-      <div className="container mx-auto text-center">
-        <h1 className="text-2xl md:text-5xl lg:text-8xl font-bold bg-gradient-text relative z-10 mb-4 md:mb-8 px-4">
-          <span className="bg-clip-text text-transparent from-blue-500 to-purple-600">
-            Where Data and Models combine for your ultimate decision.
+    <div className="py-10 md:py-3 md:px-7 pb-4 px-4 relative overflow-hidden md:mt-0 mt-[1rem] text-center">
+      <div className="container mx-auto">
+        <h1 className="text-3xl md:text-6xl leading-8 lg:text-7xl font-bold bg-gradient-text relative z-10 mb-4 md:mb-4  px-4 md:pl-0">
+          <span className="bg-clip-text leading-8 text-transparent from-blue-500 to-purple-600">
+          Opensource Realtime Datasets and Models - Your One-Stop Data and Models 
           </span>
         </h1>
+        <p className="text-lg md:text-xl text-gray-700">
+        Explore real-time opensource datasets, APIs, and LLm models in one place.
+        </p>
       </div>
     </div>
   );
